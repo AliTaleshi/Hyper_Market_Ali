22224de69fffb2e0880ebead78cb3972443e8be5
AliTaleshi <ali@DESKTOP-ASAPQPV.localdomain>
2023-04-22 13:56:29 +0330

    project base files built

---
+++ src/main/java/com/shop_center/hyper_market_ali/HyperMarketAliApplication.java
@@ -0,0 +1,13 @@
+package com.shop_center.hyper_market_ali;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HyperMarketAliApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HyperMarketAliApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shop_center/hyper_market_ali/HyperMarketAliApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shop_center.hyper_market_ali;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HyperMarketAliApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
