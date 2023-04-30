66be8cd0be8f46b0ceef1e55133aee9e2062c22a
AliTaleshi <ali@DESKTOP-ASAPQPV.localdomain>
2023-04-30 19:28:41 +0330

    some problems fixed with the customer package classes

--- src/main/java/com/shop_center/hyper_market_ali/customer/CustomerConfig.java
+++ src/main/java/com/shop_center/hyper_market_ali/customer/CustomerConfig.java
@@ -1,9 +1,28 @@
 package com.shop_center.hyper_market_ali.customer;
 
+import java.util.List;
+
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class CustomerConfig {
 
+    @Bean
+    CommandLineRunner commandLineRunner(CustomerRepository customerRepository) {
+        return arge -> {
+            Customer customer1 = new Customer(
+                    "Alex",
+                    "alex@gmail.com",
+                    25);
+
+            Customer customer2 = new Customer(
+                    "Sam",
+                    "sam@gmail.com",
+                    65);
+
+            customerRepository.saveAll(List.of(customer1, customer2));
+        };
+    }
 }
--- src/main/java/com/shop_center/hyper_market_ali/customer/CustomerService.java
+++ src/main/java/com/shop_center/hyper_market_ali/customer/CustomerService.java
@@ -3,7 +3,7 @@ package com.shop_center.hyper_market_ali.customer;
 import java.util.List;
 import java.util.Optional;
 
-import org.springframework.beans.factory.annotation.Autowired;
+// import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -11,7 +11,7 @@ public class CustomerService {
 
     private final CustomerRepository customerRepository;
 
-    @Autowired
+    // @Autowired
     public CustomerService(CustomerRepository customerRepository) {
         this.customerRepository = customerRepository;
     }
