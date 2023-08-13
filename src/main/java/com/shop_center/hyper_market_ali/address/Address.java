44ae7e3e58fce3f1c42d3f9c147e2898f4ed3035
AliTaleshi <ali@DESKTOP-ASAPQPV.localdomain>
2023-08-13 12:36:57 +0330

    Adress model encapsulation generated

--- src/main/java/com/shop_center/hyper_market_ali/address/Address.java
+++ src/main/java/com/shop_center/hyper_market_ali/address/Address.java
@@ -22,4 +22,107 @@ public class Address {
     @OneToOne(mappedBy = "address")
     private Employee employee;
 
+    public Address(Long id, String street, String city, String zipCode, Employee employee) {
+        this.id = id;
+        this.street = street;
+        this.city = city;
+        this.zipCode = zipCode;
+        this.employee = employee;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getZipCode() {
+        return zipCode;
+    }
+
+    public void setZipCode(String zipCode) {
+        this.zipCode = zipCode;
+    }
+
+    public Employee getEmployee() {
+        return employee;
+    }
+
+    public void setEmployee(Employee employee) {
+        this.employee = employee;
+    }
+
+    @Override
+    public String toString() {
+        return "Address [id=" + id + ", street=" + street + ", city=" + city + ", zipCode=" + zipCode + ", employee="
+                + employee + "]";
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        result = prime * result + ((street == null) ? 0 : street.hashCode());
+        result = prime * result + ((city == null) ? 0 : city.hashCode());
+        result = prime * result + ((zipCode == null) ? 0 : zipCode.hashCode());
+        result = prime * result + ((employee == null) ? 0 : employee.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Address other = (Address) obj;
+        if (id == null) {
+            if (other.id != null)
+                return false;
+        } else if (!id.equals(other.id))
+            return false;
+        if (street == null) {
+            if (other.street != null)
+                return false;
+        } else if (!street.equals(other.street))
+            return false;
+        if (city == null) {
+            if (other.city != null)
+                return false;
+        } else if (!city.equals(other.city))
+            return false;
+        if (zipCode == null) {
+            if (other.zipCode != null)
+                return false;
+        } else if (!zipCode.equals(other.zipCode))
+            return false;
+        if (employee == null) {
+            if (other.employee != null)
+                return false;
+        } else if (!employee.equals(other.employee))
+            return false;
+        return true;
+    }
+
 }
