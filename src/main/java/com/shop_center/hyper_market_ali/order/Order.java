89154ed4ac50eb92ee7e0c9683a7bf969fd795a6
AliTaleshi <ali@DESKTOP-ASAPQPV.localdomain>
2023-08-13 12:42:16 +0330

    Order model encapsulation generated

--- src/main/java/com/shop_center/hyper_market_ali/order/Order.java
+++ src/main/java/com/shop_center/hyper_market_ali/order/Order.java
@@ -27,4 +27,76 @@ public class Order {
     @JoinTable(name = "order_product", joinColumns = @JoinColumn(name = "order_id"), inverseJoinColumns = @JoinColumn(name = "product_id"))
     private List<Product> products = new ArrayList<>();
 
+    public Order(Long id, LocalDateTime orderDate, List<Product> products) {
+        this.id = id;
+        this.orderDate = orderDate;
+        this.products = products;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public LocalDateTime getOrderDate() {
+        return orderDate;
+    }
+
+    public void setOrderDate(LocalDateTime orderDate) {
+        this.orderDate = orderDate;
+    }
+
+    public List<Product> getProducts() {
+        return products;
+    }
+
+    public void setProducts(List<Product> products) {
+        this.products = products;
+    }
+
+    @Override
+    public String toString() {
+        return "Order [id=" + id + ", orderDate=" + orderDate + ", products=" + products + "]";
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        result = prime * result + ((orderDate == null) ? 0 : orderDate.hashCode());
+        result = prime * result + ((products == null) ? 0 : products.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Order other = (Order) obj;
+        if (id == null) {
+            if (other.id != null)
+                return false;
+        } else if (!id.equals(other.id))
+            return false;
+        if (orderDate == null) {
+            if (other.orderDate != null)
+                return false;
+        } else if (!orderDate.equals(other.orderDate))
+            return false;
+        if (products == null) {
+            if (other.products != null)
+                return false;
+        } else if (!products.equals(other.products))
+            return false;
+        return true;
+    }
+
 }
