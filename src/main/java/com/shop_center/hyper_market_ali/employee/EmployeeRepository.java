85f65ebeedfe0d0cf72f052479bac8336fa50656
AliTaleshi <ali@DESKTOP-ASAPQPV.localdomain>
2023-08-13 12:47:39 +0330

    Employee Repository added

---
+++ src/main/java/com/shop_center/hyper_market_ali/employee/EmployeeRepository.java
@@ -0,0 +1,7 @@
+package com.shop_center.hyper_market_ali.employee;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+
+}
