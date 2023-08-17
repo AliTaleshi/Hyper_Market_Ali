a0a23c98c93ba526a20fceb3715230d32531f12d
AliTaleshi <ali@DESKTOP-ASAPQPV.localdomain>
2023-08-17 20:10:30 +0330

    User Repository class  added

---
+++ src/main/java/com/shop_center/hyper_market_ali/user/UserRepository.java
@@ -0,0 +1,7 @@
+package com.shop_center.hyper_market_ali.user;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+}
