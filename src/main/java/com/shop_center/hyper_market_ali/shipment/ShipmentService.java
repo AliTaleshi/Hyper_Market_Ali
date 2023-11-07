bff1670cb02511a11d760e2f585fba2fdf4f0646
AliTaleshi <ali@DESKTOP-ASAPQPV.localdomain>
2023-11-07 11:17:32 +0330

    Develop ShipmentService Class

--- src/main/java/com/shop_center/hyper_market_ali/shipment/ShipmentService.java
+++ src/main/java/com/shop_center/hyper_market_ali/shipment/ShipmentService.java
@@ -1,9 +1,16 @@
 package com.shop_center.hyper_market_ali.shipment;
 
+import java.sql.Date;
 import java.util.List;
+import java.util.Optional;
+
 import org.springframework.stereotype.Service;
 
+import com.shop_center.hyper_market_ali.customer.Customer;
 import com.shop_center.hyper_market_ali.customer.CustomerRepository;
+
+import jakarta.persistence.EntityNotFoundException;
+import jakarta.transaction.Transactional;
 
 @Service
 public class ShipmentService {
@@ -18,5 +25,74 @@ public class ShipmentService {
 
     public List<Shipment> getShipments() {
         return shipmentRepository.findAll();
+    }
+
+    @Transactional
+    public void updateShipment(Long shipmentId, Date shipmentDate, String address, String city, String county,
+            String country, Long customerId) {
+
+        Optional<Shipment> shipmentOptional = shipmentRepository.findById(shipmentId);
+
+        if (shipmentOptional.isEmpty()) {
+            throw new EntityNotFoundException("Shipment with ID " + shipmentId + " not found");
+        }
+
+        Shipment shipment = shipmentOptional.get();
+
+        if (customerId != null) {
+            Optional<Customer> customerOptional = customerRepository.findById(customerId);
+
+            if (customerOptional.isEmpty()) {
+                throw new EntityNotFoundException("Customer with ID " + customerId + " not found");
+            }
+
+            shipment.setCustomer(customerOptional.get());
+        }
+
+        if (shipmentDate != null) {
+            shipment.setShipmentDate(shipmentDate);
+        }
+        if (address != null) {
+            shipment.setAddress(address);
+        }
+        if (city != null) {
+            shipment.setCity(city);
+        }
+        if (county != null) {
+            shipment.setCounty(county);
+        }
+        if (country != null) {
+            shipment.setCountry(country);
+        }
+
+        shipmentRepository.save(shipment);
+    }
+
+    public void deleteShipmentById(Long shipmentId) {
+        Optional<Shipment> shipmeOptional = shipmentRepository.findById(shipmentId);
+        if (shipmeOptional.isEmpty()) {
+            throw new EntityNotFoundException("Shipment with ID " + shipmentId + " not found");
+        }
+
+        shipmentRepository.deleteById(shipmentId);
+    }
+
+    public Optional<Shipment> findShipmentById(Long shipmentId) {
+        Optional<Shipment> shipmentOptional = shipmentRepository.findById(shipmentId);
+        if (shipmentOptional.isEmpty()) {
+            throw new EntityNotFoundException("Product with ID " + shipmentId);
+        }
+
+        return shipmentOptional;
+    }
+
+    public void addShipment(Shipment shipment, Long customerId) {
+        Optional<Customer> customerOptional = customerRepository.findById(customerId);
+        if (customerOptional.isEmpty()) {
+            throw new EntityNotFoundException("Category with ID " + customerId + " not found");
+        }
+
+        shipment.setCustomer(customerOptional.get());
+        shipmentRepository.save(shipment);
     }
 }
