7bf3ee0dfbd701d846116741396b52fdebc2c34f
Ali Taleshi <alitys083@gmail.com>
2024-08-18 12:41:17 +0330

    Change the directory of the UserRepository file from user_role to the user package

--- src/main/java/com/shop_center/hyper_market_ali/user/UserRepository.java
+++ src/main/java/com/shop_center/hyper_market_ali/user/UserRepository.java
@@ -1,4 +1,4 @@
-package com.shop_center.hyper_market_ali.user.user_role;
+package com.shop_center.hyper_market_ali.user;
 
 import com.shop_center.hyper_market_ali.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
