9e799e4e3529d0f64f9ed11ba01c2edd7571927c
Ali Taleshi <alitys083@gmail.com>
2024-08-18 09:58:48 +0330

    Add logging to the Category class

--- src/main/java/com/shop_center/hyper_market_ali/category/CategoryController.java
+++ src/main/java/com/shop_center/hyper_market_ali/category/CategoryController.java
@@ -2,6 +2,9 @@ package com.shop_center.hyper_market_ali.category;
 
 import java.util.List;
 
+import com.shop_center.hyper_market_ali.HyperMarketAliApplication;
+import lombok.extern.java.Log;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -10,12 +13,14 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.slf4j.Logger;
 
 @RestController
 @RequestMapping(path = "api/v1/categories")
 public class CategoryController {
 
     private final CategoryService categoryService;
+    private final Logger log = LoggerFactory.getLogger(HyperMarketAliApplication.class);
 
     public CategoryController(CategoryService categoryService) {
         this.categoryService = categoryService;
@@ -23,6 +28,7 @@ public class CategoryController {
 
     @GetMapping
     public List<Category> getCategories() {
+        log.info("Categories listed");
         return categoryService.getCategories();
     }
 
@@ -31,15 +37,18 @@ public class CategoryController {
         Category category = new Category();
         category.setName(request.getName());
         categoryService.addCategory(category);
+        log.info("Category created with id {}", request.getCategoryId());
     }
 
     @DeleteMapping("{categoryId}")
     public void deleteCustomer(@PathVariable("categoryId") Long categoryId) {
         categoryService.deleteCategoryById(categoryId);
+        log.info("Category deleted with id {}", categoryId);
     }
 
     @PutMapping("{categoryId}")
     public void updateCategory(@PathVariable("categoryId") Long categoryId, @RequestBody Category request) {
         categoryService.updateCategory(categoryId, request.getName());
+        log.info("Category updated with id {}", categoryId);
     }
 }
