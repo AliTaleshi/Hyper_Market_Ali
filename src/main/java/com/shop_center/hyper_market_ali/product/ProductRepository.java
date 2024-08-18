5bb8c3b20212af0f66eee681aac33bb60333cb26
Ali Taleshi <alitys083@gmail.com>
2024-08-18 12:37:20 +0330

    Add @Repository to all of the exsiting repositories except UserRepository, had it already

--- src/main/java/com/shop_center/hyper_market_ali/product/ProductRepository.java
+++ src/main/java/com/shop_center/hyper_market_ali/product/ProductRepository.java
@@ -4,7 +4,9 @@ import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
 
     @Query("SELECT p FROM Product p WHERE p.stockKeepingUnit = ?1")
--- src/main/java/com/shop_center/hyper_market_ali/shipment/ShipmentRepository.java
+++ src/main/java/com/shop_center/hyper_market_ali/shipment/ShipmentRepository.java
@@ -1,7 +1,9 @@
 package com.shop_center.hyper_market_ali.shipment;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ShipmentRepository extends JpaRepository<Shipment, Long> {
 
 }
